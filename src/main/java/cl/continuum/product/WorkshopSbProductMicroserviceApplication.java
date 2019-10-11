54fe1d70fc96ffd6e4370837a50e9e6d83601343
Victor Utreras J <vutreras@live.com>
2019-10-11 15:20:18 -0300

    Add cors configure

--- src/main/java/cl/continuum/product/WorkshopSbProductMicroserviceApplication.java
+++ src/main/java/cl/continuum/product/WorkshopSbProductMicroserviceApplication.java
@@ -2,6 +2,10 @@ package cl.continuum.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class WorkshopSbProductMicroserviceApplication {
@@ -10,4 +14,13 @@ public class WorkshopSbProductMicroserviceApplication {
 		SpringApplication.run(WorkshopSbProductMicroserviceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
+	}
 }
